6005f825688f74e92bd8a7d989fb8bb15855bee2
하미르 <mir_lancelot@naver.com>
2022-09-16 11:17:33 +0900

    Initial commit

---
+++ bookiz_back/src/main/java/com/ssafy/bookiz/BookizApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bookiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookizApplication.class, args);
+	}
+
+}
---
+++ bookiz_back/src/test/java/com/ssafy/bookiz/BookizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bookiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
