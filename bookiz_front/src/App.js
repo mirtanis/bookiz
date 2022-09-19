c3a765d11ffea75802784a3615cf23bf64514966
kimsoohwan <soo930810@naver.com>
2022-09-19 09:17:27 +0900

    feat: 프론트 디렉토리 구현

--- bookiz_front/src/App.js
+++ bookiz_front/src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+
+// Route
+import MainPage from "./routes/Main";
+import BookPage from "./routes/Book";
+import NotFoundPage from "./routes/NotFound";
+
+
 function App() {
   return (
-    <div className="App">   
-     BOOKIZ
-    </div>
+    <BrowserRouter>    
+      <Routes>
+          <Route path="" element={<MainPage/>}/>
+          <Route path="/book" element={<BookPage/>}/>
+          
+          <Route path="*" element={<NotFoundPage/>}/>
+      </Routes> 
+    </BrowserRouter>
   );
 }
 
---
+++ bookiz_front/src/routes/Book.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import Content from "../components/Book/Content";
+
+function BookPage() {
+  return (
+    <div>
+      <Container>
+        <Content />
+      </Container>
+    </div>
+  );
+}
+
+export default BookPage;
+
+const Container = styled.div`
+  width: 1440px;
+  margin: auto;
+`;
