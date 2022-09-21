00c4e9af4af4e3052ee9cc67da4955d7b7093cff
이도경 <zz132456zz@kakao.com>
2022-09-21 11:03:32 +0900

    feat : tts 음성 파일 생성

--- bookiz_ai/tts/views.py
+++ bookiz_ai/tts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from gtts import gTTS
 
-# Create your views here.
+def tts(request):
+    if request.method == 'GET':
+        text = request.GET.get('text')
+        print(text)
+        file_name = "sample.mp3"
+        tts_ko = gTTS(text=text, lang='ko')
+        tts_ko.save(file_name)
+        return HttpResponse("Hello, tts")
