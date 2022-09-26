ce48ae05baa21b2277c104a0fbf54661650b93fb
이도경 <zz132456zz@kakao.com>
2022-09-26 11:43:38 +0900

    fix : CORS URL 추가

--- bookiz_ai/bookiz_ai/settings.py
+++ bookiz_ai/bookiz_ai/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://localhost:3000',
 ]
 
 # Application definition
