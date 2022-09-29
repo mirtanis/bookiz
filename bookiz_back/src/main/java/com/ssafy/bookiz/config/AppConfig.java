79e9f8480ca21655ab58d354e764dd246c91aa78
jelee6613 <jelee6613@gmail.com>
2022-09-29 16:02:34 +0900

    fix : webconfig

--- bookiz_back/src/main/java/com/ssafy/bookiz/config/AppConfig.java
+++ bookiz_back/src/main/java/com/ssafy/bookiz/config/AppConfig.java
@@ -3,9 +3,6 @@ package com.ssafy.bookiz.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -13,17 +10,6 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
-    }
-
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
     }
 
 }
